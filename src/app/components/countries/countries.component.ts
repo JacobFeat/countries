004dac4e388efef3672598fda43d73e57e0c2afc
JacobFeat <jakubk198@gmail.com>
2022-10-16 11:37:19 +0200

    add mobile version for filters

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -4,6 +4,7 @@ import { catchError, EMPTY, of, Subscription, tap } from 'rxjs';
 import { Regions } from 'src/app/components/countries/enums/regions';
 import { CountriesService } from 'src/app/common/services/countries.service';
 import { SpinnerService } from 'src/app/common/services/spinner.service';
+import { CountriesStore, Country } from './models/country';
 
 @Component({
   selector: 'app-countries',
@@ -11,13 +12,24 @@ import { SpinnerService } from 'src/app/common/services/spinner.service';
   styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit, OnDestroy {
-  countries?: any;
+  countries!: Country[];
   isAnyCountry = true;
   currentRegion?: Regions;
 
   isSpinnerVisible!: boolean;
 
   subs: Subscription[] = [];
+
+  countriesStore: CountriesStore = {
+    all: [],
+    asia: [],
+    africa: [],
+    americas: [],
+    europe: [],
+    oceania: [],
+  };
+
+  tempCountries!: Country[];
 
   constructor(
     private countriesService: CountriesService,
@@ -46,10 +58,17 @@ export class CountriesComponent implements OnInit, OnDestroy {
   }
 
   fetchCountries(): void {
+    if (this.countriesStore.all.length) {
+      this.countries = this.countriesStore.all;
+      this.isSpinnerVisible = false;
+      return;
+    }
+
     this.subs.push(
       this.countriesService.all().subscribe((countries) => {
         if (countries) this.isSpinnerVisible = false;
         this.countries = countries;
+        this.countriesStore.all = countries;
       })
     );
   }
@@ -63,29 +82,42 @@ export class CountriesComponent implements OnInit, OnDestroy {
   }
 
   searchCountriesByName(countryName: string): void {
-    if (!countryName) {
-      return this.fetchCountries();
-    }
-    this.subs.push(
-      this.countriesService
-        .searchCountriesByName(countryName)
-        .pipe(
-          catchError(() => {
-            this.isAnyCountry = false;
-            return EMPTY;
-          }),
-          tap(() => (this.isAnyCountry = true))
-        )
-        .subscribe((countries) => (this.countries = countries))
-    );
+    // if (!countryName) {
+    //   return this.fetchCountries();
+    // }
+    // this.subs.push(
+    //   this.countriesService
+    //     .searchCountriesByName(countryName)
+    //     .pipe(
+    //       catchError(() => {
+    //         this.isAnyCountry = false;
+    //         return EMPTY;
+    //       }),
+    //       tap(() => (this.isAnyCountry = true))
+    //     )
+    //     .subscribe((countries) => (this.countries = countries))
+    // );
+    const foundCountries = this.countries.filter(({ name }) => {
+      const lowerCaseName = name.common?.toLowerCase();
+      return lowerCaseName?.includes(countryName.toLowerCase());
+    });
+    this.countries = foundCountries;
   }
 
   searchCountriesByRegion(regionName: string): void {
+    if (this.countriesStore[regionName as keyof CountriesStore].length) {
+      this.countries =
+        this.countriesStore[regionName as keyof typeof this.countriesStore];
+      this.isSpinnerVisible = false;
+      return;
+    }
+
     this.subs.push(
       this.countriesService
         .searchCountriesByRegion(regionName)
         .subscribe((countries) => {
           if (countries) this.isSpinnerVisible = false;
+          this.countriesStore[regionName as keyof CountriesStore] = countries;
           this.countries = countries;
         })
     );
--- src/app/components/countries/models/country.ts
+++ src/app/components/countries/models/country.ts
@@ -1,31 +1,40 @@
 export interface Country {
-    name: Names;
-    nativeName: NativeName;
-    population: number;
-    region: string;
-    subregion: string;
-    capital: string[];
-    tld: string[];
-    currencies: any[];
-    languages: any;
-    borders: string[];
-    flags: Flags;
-    cioc?: string;
-    ccn3?: string;
+  name: Names;
+  nativeName: NativeName;
+  population: number;
+  region: string;
+  subregion: string;
+  capital: string[];
+  tld: string[];
+  currencies: any[];
+  languages: any;
+  borders: string[];
+  flags: Flags;
+  cioc?: string;
+  ccn3?: string;
 }
 
 export interface Flags {
-    png?: string;
-    svg?: string;
+  png?: string;
+  svg?: string;
 }
 
 export interface Names {
-    common?: string;
-    official?: string;
-    nativeName?: any;
+  common?: string;
+  official?: string;
+  nativeName?: any;
 }
 
 export interface NativeName {
   common: string;
   official: string;
+}
+
+export interface CountriesStore {
+  all: Country[];
+  asia: Country[];
+  africa: Country[];
+  americas: Country[];
+  europe: Country[];
+  oceania: Country[];
 }
